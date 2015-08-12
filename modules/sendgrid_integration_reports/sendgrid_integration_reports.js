71efec54274a683b8b4d7b9e374bcb5b8d03ba1d
Brady Owens <info@fastglass.net>
2015-08-12 09:18:05 +0200

    Google Visualization chart added for spam reports

--- modules/sendgrid_integration_reports/sendgrid_integration_reports.js
+++ modules/sendgrid_integration_reports/sendgrid_integration_reports.js
@@ -17,6 +17,10 @@
         dataTableVol.addColumn('number', Drupal.t('Clicks'));
         dataTableVol.addColumn('number', Drupal.t('Delivered'));
 
+        var dataTableSpa = new google.visualization.DataTable();
+        dataTableSpa.addColumn('datetime', Drupal.t('Date'));
+        dataTableSpa.addColumn('number'), Drupal.t('Spam');
+
         for (var key in settings.sendgrid_integration_reports.global) {
           dataTableVol.addRow([
             new Date(settings.sendgrid_integration_reports.global[key]['date']),
@@ -24,15 +28,22 @@
             settings.sendgrid_integration_reports.global[key]['clicks'],
             settings.sendgrid_integration_reports.global[key]['delivered']
           ]);
+          dataTableSpa.addRow([
+            new Date(settings.sendgrid_integration_reports.global[key]['date']),
+            settings.sendgrid_integration_reports.global[key]['spam_reports']
+          ]);
         }
 
         var options = {
           pointSize: 5,
-          hAxis: {format: 'MM/dd/y'}
+          hAxis: {format: 'y/MM/dd'}
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('sendgrid-global-volume-chart'));
-        chart.draw(dataTableVol, options);
+        var chart0 = new google.visualization.LineChart(document.getElementById('sendgrid-global-volume-chart'));
+        chart0.draw(dataTableVol, options);
+
+        var chart1 = new google.visualization.LineChart(document.getElementById('sendgrid-global-spam-chart'));
+        chart1.draw(dataTableSpa, options);
       }
     }
   }
