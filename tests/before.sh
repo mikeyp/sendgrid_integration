df3a77fc06c4b1d1faf26415f83b475dbc73f662
Brady Owens <info@fastglass.net>
2016-02-24 21:15:56 +0100

    More comments for Travis degugging.

--- tests/before.sh
+++ tests/before.sh
@@ -9,14 +9,16 @@ drupal_ti_ensure_drupal
 
 # Enable simpletest module.
 cd "$DRUPAL_TI_DRUPAL_DIR"
-echo "Drush Enable Simpletest module"
+echo "DRUPAL TI - Drush Enable Simpletest module"
 drush --yes en simpletest
 
 # Ensure the module is linked into the code base and enabled.
-echo "Ensure the module is linked into the code base and enabled"
+echo "DRUPAL TI - Ensure the module is linked into the code base and enabled"
 drupal_ti_ensure_module
 
 # Clear caches and run a web server.
+echo "DRUPAL TI - Clear caches"
 drupal_ti_clear_caches
+echo "DRUPAL TI - Run a web server"
 drupal_ti_run_server
 
